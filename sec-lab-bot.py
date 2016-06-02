ae3aa5d78eab1443a7d394b54b94636da02e17bd
Max Zinkus <mzinkus@calpoly.edu>
2016-06-02 15:45:32 -0700

    Add TODOs

--- sec-lab-bot.py
+++ sec-lab-bot.py
@@ -11,25 +11,40 @@ FIGLET_WIDTH = 256
 CLOSED = 0
 OPEN = 1
 
+def send_open_request():
+    pass # TODO
+
+def send_close_request():
+    pass # TODO
+
+"""
+        TODO
+    ************
+    * bold     *
+    * color    *
+    * requests *
+    * async    *
+    ************
+"""
+
 def main(win):
     fig = Figlet(font=FIGLET_FONT, width=FIGLET_WIDTH)
-    win.nodelay(1)
     state = CLOSED
     while True:          
         try:                 
-            key = win.getkey()         
-            win.clear()
-            if state == CLOSED:
-                banner = fig.renderText('OPEN')
-            elif state == OPEN:
-                banner = fig.renderText('CLOSED')
-            win.addstr(banner)
-            state ^= 1
+            key = win.getch()         
+            if key != -1:
+                win.clear()
+                if state == CLOSED:
+                    banner = fig.renderText('The Lab is\nOPEN :)')
+                    send_open_request()
+                elif state == OPEN:
+                    banner = fig.renderText('The Lab is\nCLOSED :(')
+                    send_close_request()
+                win.addstr(banner)
+                state ^= 1
         except KeyboardInterrupt:
             return
-        except Exception as e:
-            # No input   
-            pass
 
 if __name__ == '__main__':
     curses.wrapper(main)
