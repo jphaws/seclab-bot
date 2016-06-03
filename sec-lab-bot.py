d8d6863450d5fb9080e957c2aac43a6ae4930e6a
Max Zinkus <mzinkus@calpoly.edu>
2016-06-02 19:29:36 -0700

    Code cleanup

--- sec-lab-bot.py
+++ sec-lab-bot.py
@@ -2,90 +2,107 @@
 
 import sys
 import hmac
+import time
 import curses
 import socket
 import logging
-from time import time
 from pyfiglet import Figlet
+
 
 logging.basicConfig(filename='client.log', level=logging.DEBUG)
 
 FIGLET_FONT = 'doh'
 FIGLET_WIDTH = 256
 
+fig = Figlet(font=FIGLET_FONT, width=FIGLET_WIDTH)
+
+OPEN_BANNER = fig.renderText('The Lab is\nOPEN :)'.strip())
+CLOSE_BANNER = fig.renderText('The Lab is\nCLOSED :('.strip())
+
+
 SOCKET_HOST = "127.0.0.1"
 SOCKET_PORT = 8080
+
 
 STATE_CLOSED = 0
 STATE_OPEN = 1
 
-class NetworkException(Exception):
-    pass
 
 def network_trycatch(reqtype):
+    """ Takes a request type (open/close) and returns a decorator """
     def decorator(fn):
+        """ Takes a function and calls it, wrapped in try/catch, given a connection object """
         def inner():
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
                     conn.connect((SOCKET_HOST, SOCKET_PORT))
                     fn(conn)
-            except OSError as e:
-                logging.warning(str(e))
             except Exception as e:
-                logging.warning(str(e))
+                logging.warning(log(str(e)))
         return inner
     return decorator
 
+
 def timestamp():
+    """ 8-byte timestamp for the request """
     return int.to_bytes(time().__trunc__(), 8, 'little')
+
+
+def log(s):
+    """ Take a string and preprend a local timestamp for logging """
+    return "[" + time.asctime() + "] " + s
+
 
 @network_trycatch("open")
 def send_open_request(conn=None):
-    logging.info("client sent open request")
-    conn.sendall(b"Hello, World! I'm open!\n")
+    """ Send an 'OPEN' request to the server """
+    logging.info(log("client sent open request"))
+    conn.sendall(log("Hello, World! I'm open!\n").encode())
 
 
 @network_trycatch("close")
 def send_close_request(conn=None):
-    logging.info("client sent close request")
-    conn.sendall(b"Hello, World! I'm closed.\n")
+    """ Send a 'CLOSE' request to the server """
+    logging.info(log("client sent close request"))
+    conn.sendall(log("Hello, World! I'm closed.\n").encode())
 
-
-"""
-        TODO
-    ************
-    * bold     *
-    * color    *
-    * requests *
-    ************
-"""
 
 def main(win):
-    logging.info("client init")
-    fig = Figlet(font=FIGLET_FONT, width=FIGLET_WIDTH)
+    """ ncurses loop, banner generation """
+    logging.info(log("client init"))
+
     state = STATE_CLOSED
+
+    # ncurses init
     win.clear()
+    curses.curs_set(0)
     win.nodelay(0)
     win.addstr("Any key to toggle, Control-c to quit")
     win.refresh()
+    
     while True:          
         try:                 
-            key = win.getch()         
+            key = win.getch()
             win.clear()
             if state == STATE_CLOSED:
                 send_open_request()
-                banner = fig.renderText('The Lab is\nOPEN :)'.strip())
+                try:
+                    win.addstr(OPEN_BANNER, curses.A_BOLD)
+                except Exception as e:
+                    logging.warning(str(e))
             elif state == STATE_OPEN:
                 send_close_request()
-                banner = fig.renderText('The Lab is\nCLOSED :('.strip())
-            try:
-                win.addstr(banner)
-            except Exception as e:
-                logging.warning(str(e))
+                try:
+                    win.addstr(CLOSE_BANNER, curses.A_BOLD)
+                except Exception as e:
+                    logging.warning(str(e))
+
             state ^= 1
+
         except KeyboardInterrupt:
-            logging.info("client exit")
+            logging.info(log("client exit"))
             return
+
 
 if __name__ == '__main__':
     curses.wrapper(main)
