86cb1f5d2cf6cb954eb31e142a7b754f4be2a4a4
Stephen Parkinson <smparkin@calpoly.edu>
2022-01-26 20:05:14 -0800

    update bot for api

--- sec-lab-bot.py
+++ sec-lab-bot.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 """ SecLab Bot client """
 
+import json
 import sys
-import ssl
-import hmac
+import os
 import time
 import curses
-import socket
 import logging
-import hashlib
-from base64 import b64decode as b64d, b64encode as b64e
+from typing import Optional
+import requests
 from pyfiglet import Figlet
 
-KEY_FILE = 'files/psk.b64'
 
 LOG_FILE = 'files/client.log'
 MAX_LOG_ENTRIES = 1024
 
+API_USER = os.environ.get("API_USER")
+API_PASS = os.environ.get("API_PASS")
+API_URL = "https://cpsecurity.club/api/v1/status"
+#API_URL = "http://localhost:3000/api/v1/status"
+
 FIGLET_FONT = 'doh'
-#FIGLET_WIDTH = 256
 FIGLET_WIDTH = 154
 FIGLET = Figlet(font=FIGLET_FONT, width=FIGLET_WIDTH)
 BANNER_OPEN = FIGLET.renderText('Lab is OPEN :)'.strip())
@@ -34,156 +36,33 @@ with open('coffee.txt', 'r') as c:
 
 BANNER_COFFEE = FIGLET.renderText(('Out for\n').strip()) + coffee
 
-SOCKET_HOST = "thewhitehat.club"
-#SOCKET_HOST = "localhost"
-SOCKET_PORT = 3737
-
-BYTE_ORDER = 'big'  # endianness
-
-MAX_AGE = 10  # seconds
-
-
-class SecLabBotException(Exception):
-    """ Generic Exception
-        for SecLabBot-specific problems
-    """
-    pass
-
-
-# Some magic numbers with pre-agreed meanings
-# for the server and client, AKA a protocol
-FLAG_OPEN_REQ = 0xFF
-FLAG_CLOSE_REQ = 0x00
-FLAG_KEYGEN_REQ = 0xAA
-FLAG_ALL_GOOD = 0xFF
-FLAG_KEYGEN_ACK = 0x55
-FLAG_COFFEE_REQ = 0xCC
-FLAG_FIRE_REQ = 0xFC
-
 STATE_CLOSED = 0
 STATE_OPEN = 1
 
-EXIT_SUCCESS = True
-EXIT_FAILURE = False
-
-DEBUG = SOCKET_HOST in ["localhost", "127.0.0.1"]
-
-# Pin TLS and only use good ciphers
-SSL_CA_FILE = 'files/pinned.pem'
-SSL_CIPHER_LIST =\
- "AESGCM:AESCCM:AES256:SUITEB192:SUITEB128:CHACHA20" if not DEBUG else "ALL"
+PRE_DEF_COLORS = {
+    "open": "brightgreen",
+    "closed": "red",
+    "fire": "orange",
+    "coffee": "8f7369"
+}
 
 
-def read_key_from_file():
-    """ Read saved HMACing key """
-    try:
-        with open(KEY_FILE, 'rb') as key_file:
-            return b64d(key_file.read())
-    except:
-        logging.warning("error reading from key file")
-
-
-def write_key_to_file(key):
-    """ Write new HMACing key """
-    try:
-        with open(KEY_FILE, 'wb') as key_file:
-            key_file.write(b64e(key))
-    except:
-        logging.warning("error writing to key file")
-
-
-KEY = read_key_from_file()
-
-
-def ssl_wrap_socket(sock):
-    """ Takes a socket, spits out an SSL-enabled socket
-        using horrible security if connecting to localhost (testing)
-    """
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    if DEBUG:
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
-    else:
-        context.verify_mode = ssl.CERT_NONE
-    context.set_ciphers(SSL_CIPHER_LIST)
-    return context.wrap_socket(sock, server_hostname=SOCKET_HOST)
-
-
-def wire_decode_int(data, sgn=False):
-    """ decode bytes to int from the wire """
-    return int.from_bytes(data, byteorder=BYTE_ORDER, signed=sgn)
-
-
-def wire_encode_int(i, size, sgn=False):
-    """ encode int to bytes for the wire """
-    return i.to_bytes(size, byteorder=BYTE_ORDER, signed=sgn)
-
-
-def timestamp():
-    """ timestamp (to be encoded or compared) """
-    return time.time().__trunc__()
-
-
-def timestamp_bytes():
-    """ 8-byte timestamp for the request (for freshness)
-         keep at 8 bytes to avoid the 2038 problem
-    """
-    return wire_encode_int(timestamp(), 8, sgn=True)
-
-
-def timestamp_verify(tdata):
-    """ Validate timestamp freshness """
-    return (timestamp() - wire_decode_int(tdata, sgn=True)) <= MAX_AGE
-
-
-def ssl_request(reqtype):
-    """ Takes a request type (open/close/keygen) and makes the request """
+def api_request(reqtype, statcolor: Optional[str] = None):
+    """ Takes a request type (open, closed, etc.) and makes the request """
     logging.info("client sent " + reqtype + " request")
+    if statcolor is None:
+        if reqtype in PRE_DEF_COLORS:
+            statcolor = PRE_DEF_COLORS[reqtype]
+        else:
+            statcolor = "purple"
+    data = {"StatusName": reqtype, "StatusColor": statcolor}
     try:
-        with ssl_wrap_socket(
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ) as conn:
-            conn.connect((SOCKET_HOST, SOCKET_PORT))
-            conn.sendall(make_request(reqtype))
-            if reqtype in ["open", "close", "coffee", "fire"]:
-                if wire_decode_int(conn.recv(1)) != FLAG_ALL_GOOD:
-                    raise SecLabBotException("client received bad response")
-            elif reqtype in ["keygen"]:
-                if wire_decode_int(conn.recv(1)) != FLAG_KEYGEN_ACK:
-                    raise SecLabBotException("client received bad response")
-                if not timestamp_verify(conn.recv(8)):
-                    raise SecLabBotException("client received stale response")
-                write_key_to_file(conn.recv(32))
-            else:
-                raise SecLabBotException("Unknown request type in ssl_request")
-        return EXIT_SUCCESS
-    except Exception as exc:
-        logging.warning(str(exc) + " during " + reqtype + " request")
-        return EXIT_FAILURE
-
-
-def make_request(reqtype):
-    """ Creates an HMAC'd request message bytestring """
-    data = b""
-    val = None
-    if reqtype == "open":
-        val = FLAG_OPEN_REQ
-    elif reqtype == "close":
-        val = FLAG_CLOSE_REQ
-    elif reqtype == "coffee":
-        val = FLAG_COFFEE_REQ
-    elif reqtype == "fire":
-        val = FLAG_FIRE_REQ
-    elif reqtype == "keygen":
-        val = FLAG_KEYGEN_REQ
-    else:
-        logging.error("Unknown request type in make_request")
-        raise ValueError
-    data += wire_encode_int(val, 1, sgn=False)
-    data += timestamp_bytes()
-    mac = hmac.new(KEY, msg=data, digestmod=hashlib.sha256)
-    data += mac.digest()
-    return data
+        resp = requests.post(API_URL, json=data, auth=(API_USER, API_PASS))
+    except:
+        return False
+    if resp.status_code != 200:
+            return False
+    return True
 
 
 def ncurses_write(win, msg):
@@ -220,22 +99,22 @@ def main(win):
                 continue
             if ch == ord('f') and state == STATE_OPEN: # the lab is burning
                 reqtype = "fire"
-                success = ssl_request(reqtype)
+                success = api_request(reqtype)
                 if success:
                     ncurses_write(win, BANNER_FIRE)
             elif ch == ord('c') and state == STATE_OPEN: # coffee mode
                 reqtype = "coffee"
-                success = ssl_request(reqtype)
+                success = api_request(reqtype)
                 if success:
                     ncurses_write(win, BANNER_COFFEE)
             elif state == STATE_CLOSED:
                 reqtype = "open"
-                success = ssl_request(reqtype)
+                success = api_request(reqtype)
                 if success:
                     ncurses_write(win, BANNER_OPEN)
             elif state == STATE_OPEN:
-                reqtype = "close"
-                success = ssl_request(reqtype)
+                reqtype = "closed"
+                success = api_request(reqtype)
                 if success:
                     ncurses_write(win, BANNER_CLOSE)
             if success:
@@ -253,15 +132,11 @@ def main(win):
 
 def show_help():
     """ Print help information """
-    print("SecLab Bot usage: python3 sec-lab-bot.py [--keygen]")
+    print("SecLab Bot usage: python3 sec-lab-bot.py")
     print()
-    print("The following files are required for operation:")
-    print("\tLOG_FILE\t[files/client.log]")
-    print("\tKEY_FILE\t[files/psk.b64]")
-    print("\tSSL_CERT_FILE\t[files/pinned.pem]")
-    print()
-    print("The --keygen option will request a new PSK from the server")
-    print()
+    print("The following env vars are required for operation:")
+    print("\tAPI_USER\t[api username]")
+    print("\tAPI_PASS\t[api password]")
     print("Use any key to toggle open/close, or control-c to quit")
 
 
@@ -286,14 +161,6 @@ if __name__ == '__main__':
                         format='[%(asctime)s] %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
                         level=logging.DEBUG)
-
-    if "--keygen" in sys.argv:
-        if ssl_request("keygen"):
-            logging.info("Key rotated")
-            sys.exit(0)
-        else:
-            logging.critical("Key rotate failed!")
-            sys.exit(1)
 
     curses.wrapper(main)
     logging.shutdown()
